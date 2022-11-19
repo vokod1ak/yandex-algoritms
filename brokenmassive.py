86a6978bf4d35cbe25c4e312edd171a885d57295
Khabarov Konstantin <ya.ibmer@yandex.ru>
2022-11-19 11:21:00 +0300

    Final tasks for Sprint 13 minor

--- brokenmassive.py
+++ brokenmassive.py
@@ -1,9 +1,23 @@
-def broken_search(nums, target) -> int:
-    #  Your code
-    #  “ヽ(´▽｀)ノ”
-    pass
+# ID удачной посылки: https://contest.yandex.ru/contest/24735/run-report/75315748/
+def broken_search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
 
 
 def test():
-    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
-    assert broken_search(arr, 5) == 6
+    arr = [0, 2, 6, 7, 8, 9, 10]
+    assert broken_search(arr, 9) == 5
